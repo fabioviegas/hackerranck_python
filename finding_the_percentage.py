5602076a336ad83c01488161c037a12097b378a1
Fabio Viegas <fabioviegas.rs@gmail.com>
2020-09-08 10:37:16 -0300

    Commit pelo vscode

--- finding_the_percentage.py
+++ finding_the_percentage.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
 
     query_name = input()
     print("{:.2f}".format(sum(student_marks.get(query_name))/len(student_marks.get(query_name))))
+    #teste feito pelo vscode
 
