a27d786c46d790cd97e0e11b71be55884fc15de0
Fabio Viegas <fabioviegas.rs@gmail.com>
2020-10-24 15:24:35 -0300

    Exemplo de uso do beautiful soup

---
+++ scraping.py
@@ -0,0 +1,13 @@
+'''Pegar o um arquivo .html e ver quantas col2 tem'''
+import requests
+from bs4 import BeautifulSoup
+
+
+with open('/home/fabio/Projetos/git/hackerranck_python/tipo_peticao_judicial.html', 'r',  encoding='iso8859-1') as f:
+
+    contents = f.read()
+
+    soup = BeautifulSoup(contents, 'html.parser')
+    print(len(soup.find_all('td', class_='col2')))
+    for td in soup.find_all('td', class_='col2'):
+        print(td.getText())
